aadcf06ce49b906130b7dd6fe40fd7b8876cbfc2
Rio Gunawan <riogunawan29@gmail.com>
2018-09-13 15:18:10 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import { AppRegistry } from 'react-native';
+// import HelloWorld from './components/HelloWorld';
+// import Robot from './components/Robot';
+// import MultipleGreetings from './components/MultipleGreetings';
+// import TextBlink from './components/TextBlink';
+// import flexDimension from './components/flexDimension';
+// import inputan from './components/inputan';
+import basicFlatList from './components/basicFlatList';
+// import HorizontalFlatList from './components/HorizontalFlatList';
+// import BasicSectionList from './components/BasicSectionList';
+// import LifeCycleComponent from './components/LifeCycleComponent';
+// import App from './App';
+
+AppRegistry.registerComponent('tutorialproject', () => basicFlatList);
